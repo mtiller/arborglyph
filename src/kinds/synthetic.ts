1c9209aa16e0570805dcaa5b5f0d26fa0897f9e0
Michael Tiller <michael.tiller@gmail.com>
2021-11-23 11:39:24 -0500

    Added wrappers for synthetic evaluators

--- src/kinds/synthetic.ts
+++ src/kinds/synthetic.ts
@@ -14,9 +14,14 @@ export function defineSynthetic<T extends object, R>(
   };
 }
 
+export type SyntheticEvaluationWrapper<T> = <S extends T, R>(
+  e: SyntheticAttributeEvaluator<S, R>
+) => SyntheticAttributeEvaluator<S, R>;
+
 export interface SyntheticOptions<T, R> {
+  wrapper?: SyntheticEvaluationWrapper<T>;
   memoize?: "no" | "weakmap" | "lru";
-  lru?: LRUCache.Options<T, R>;
+  lru?: LRUOptions;
 }
 
 /**
@@ -38,44 +43,60 @@ export function reifySyntheticAttribute<T extends object, R>(
   /** Check what level of memoization is requested */
   const memo = opts.memoize ?? "no";
 
-  if (memo === "weakmap" || memo === "lru") {
-    /** If memoization is requested, first create storage for memoized values. */
-    const resultStorage =
-      memo === "weakmap" ? new WeakMap<T, R>() : new LRUCache<T, R>(opts.lru);
-    const childStorage = memo === "weakmap" ? new WeakMap<T, T[]>() : null;
-
-    /**
-     * Now create a special memoized wrapper that checks for memoized values and
-     * caches any attributes actually evaluated.
-     **/
-    const memoizeEvaluator: SyntheticAttributeEvaluator<T, R> = (args) => {
-      const children = args.children.map((c) => c.node);
-      const cachedChildren = childStorage?.get(args.node) ?? children;
-      if (
-        resultStorage.has(args.node) &&
-        children.length === cachedChildren.length &&
-        children.every((c, i) => c === cachedChildren[i])
-      )
-        return resultStorage.get(args.node) as R;
-      const ret = evaluator(args);
-      resultStorage.set(args.node, ret);
-      childStorage?.set(args.node, children);
-      return ret;
-    };
-
-    /** Create an attribute function using a memoizing attribute evaluator */
-    const memoed = baseSyntheticAttributeCalculation(
+  if (memo === "weakmap") {
+    return baseSyntheticAttributeCalculation(
       root,
       list,
-      memoizeEvaluator
+      weakmapWrapper(evaluator)
     );
-
-    // NB - I don't see any value to precomputing of synthetic attributes.  It doesn't really
-    // avoid the search that precomputing inherited attributes does.
-
-    // Return the memoizing attribute function.
-    return memoed;
   }
+
+  if (memo === "lru") {
+    return baseSyntheticAttributeCalculation(
+      root,
+      list,
+      lruWrapper(opts.lru)(evaluator)
+    );
+  }
+
+  // if (memo === "weakmap" || memo === "lru") {
+  //   /** If memoization is requested, first create storage for memoized values. */
+  //   const resultStorage =
+  //     memo === "weakmap" ? new WeakMap<T, R>() : new LRUCache<T, R>(opts.lru);
+  //   const childStorage = memo === "weakmap" ? new WeakMap<T, T[]>() : null;
+
+  //   /**
+  //    * Now create a special memoized wrapper that checks for memoized values and
+  //    * caches any attributes actually evaluated.
+  //    **/
+  //   const memoizeEvaluator: SyntheticAttributeEvaluator<T, R> = (args) => {
+  //     const children = args.children.map((c) => c.node);
+  //     const cachedChildren = childStorage?.get(args.node) ?? children;
+  //     if (
+  //       resultStorage.has(args.node) &&
+  //       children.length === cachedChildren.length &&
+  //       children.every((c, i) => c === cachedChildren[i])
+  //     )
+  //       return resultStorage.get(args.node) as R;
+  //     const ret = evaluator(args);
+  //     resultStorage.set(args.node, ret);
+  //     childStorage?.set(args.node, children);
+  //     return ret;
+  //   };
+
+  //   /** Create an attribute function using a memoizing attribute evaluator */
+  //   const memoed = baseSyntheticAttributeCalculation(
+  //     root,
+  //     list,
+  //     memoizeEvaluator
+  //   );
+
+  //   // NB - I don't see any value to precomputing of synthetic attributes.  It doesn't really
+  //   // avoid the search that precomputing inherited attributes does.
+
+  //   // Return the memoizing attribute function.
+  //   return memoed;
+  // }
   /** Build a function that can compute our attribute */
   return baseSyntheticAttributeCalculation(root, list, evaluator);
 }
@@ -136,3 +157,56 @@ export interface SyntheticArg<T, R> {
 }
 
 export type SyntheticAttributeEvaluator<T, R> = (x: SyntheticArg<T, R>) => R;
+
+export const weakmapWrapper: SyntheticEvaluationWrapper<object> = <
+  T extends object,
+  R
+>(
+  evaluator: SyntheticAttributeEvaluator<T, R>
+): SyntheticAttributeEvaluator<T, R> => {
+  const resultStorage = new WeakMap<T, R>();
+  const childStorage = new WeakMap<T, T[]>();
+
+  return (args) => {
+    const children = args.children.map((c) => c.node);
+    const cachedChildren = childStorage?.get(args.node) ?? children;
+    if (
+      resultStorage.has(args.node) &&
+      children.length === cachedChildren.length &&
+      children.every((c, i) => c === cachedChildren[i])
+    )
+      return resultStorage.get(args.node) as R;
+    const ret = evaluator(args);
+    resultStorage.set(args.node, ret);
+    childStorage?.set(args.node, children);
+    return ret;
+  };
+};
+
+export interface LRUOptions {
+  max?: number;
+}
+
+export function lruWrapper(
+  opts?: LRUOptions
+): SyntheticEvaluationWrapper<object> {
+  return <T extends object, R>(
+    evaluator: SyntheticAttributeEvaluator<T, R>
+  ): SyntheticAttributeEvaluator<T, R> => {
+    const resultStorage = new LRUCache<T, R>(opts);
+
+    return (args) => {
+      const children = args.children.map((c) => c.node);
+      const cachedChildren = children;
+      if (
+        resultStorage.has(args.node) &&
+        children.length === cachedChildren.length &&
+        children.every((c, i) => c === cachedChildren[i])
+      )
+        return resultStorage.get(args.node) as R;
+      const ret = evaluator(args);
+      resultStorage.set(args.node, ret);
+      return ret;
+    };
+  };
+}
