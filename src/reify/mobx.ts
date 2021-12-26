fb69d4c80334a2052be4372dea5590801bdc5807
Michael Tiller <michael.tiller@gmail.com>
2021-12-26 11:30:21 -0500

    Got rid of old mobx helpers

--- src/reify/mobx.ts
+++ src/reify/mobx.ts
@@ -30,6 +30,33 @@ import { reifySyntheticAttribute } from "./synthetic";
 export interface MobxReifierOptions extends StandardReifierOptions {
   computed: { requiresReaction?: boolean; keepAlive?: boolean };
 }
+
+/**
+ * THE TRICK
+ *
+ * So I think I may have finally cracked the code on how to use mobx observable
+ * values without really having to change much in the tree walking.  The trick
+ * seems to be that we need to wrap everything in lazily computed
+ * `IComputedValue`s.  So instead of our attributes being functions that return
+ * `R`, they should return `IComputeValue<R>`.
+ *
+ * This file contains some simple function that transoform attribute evaluation
+ * functions into ones that provide mobx compatible `IComputedValue` values.
+ *
+ * NB - Regarding caching, if you do not plan to make any structural changes to
+ * the tree, you can probably enable "weakmap" caching.  But if there are going
+ * to be structural changes, you'll wind up "memoizing" information about the
+ * structure that will lead to errors when evaluating (because the traversal
+ * will be based on stale information).
+ *
+ * Along these lines, the issue with "weakmap" caching is that the evaluation
+ * code can't detect when the children have changed and that information gets
+ * backed into the "args".  If that doesn't get recomputed, then calls to
+ * computedValue.get() lead to traversal over stale elements of the tree.  If,
+ * however, children were also computed values (automatically recalculated when
+ * observables changed) or otherwise invalidated on structural changes, then we
+ * could avoid stale data getting memoized.
+ */
 export class MobxReifier implements Reifier<object> {
   protected syntheticOptions: Partial<CommonSyntheticOptions>;
   protected inheritedOptions: Partial<CommonInheritedOptions>;
