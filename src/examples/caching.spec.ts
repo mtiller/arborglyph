0c7bf9527d4a3c851b5f2f3b9ebd81ef0369b389
Michael Tiller <michael.tiller@gmail.com>
2021-12-28 17:04:23 -0500

    Same test for three different cases

--- src/examples/caching.spec.ts
+++ src/examples/caching.spec.ts
@@ -5,59 +5,78 @@
  **/
 
 import { Arbor } from "..";
+import { ArborPlugin } from "../plugin";
 import { CounterPlugin } from "../plugins/counter";
 import {
   findChild,
   fork,
   indexedBinaryChildren,
+  SimpleBinaryTree,
   symTree1,
   symTree2,
 } from "../testing";
 import { repminCluster } from "./observable_repmin.test";
 import { evalMin } from "./repmin_attrs";
 
+interface CaseData {
+  memoized: boolean;
+  immutable: boolean;
+  plugins: ArborPlugin<SimpleBinaryTree>[];
+  sub: number;
+  total1: number;
+  total2: number;
+  total3: number;
+  total4: number;
+  total5: number;
+}
+
+const cases: CaseData[] = [
+  {
+    memoized: false,
+    immutable: true,
+    plugins: [],
+    sub: 3,
+    total1: 10,
+    total2: 17,
+    total3: 24,
+    total4: 31,
+    total5: 38,
+  },
+  {
+    memoized: true,
+    immutable: true,
+    plugins: [],
+    sub: 3,
+    total1: 7,
+    total2: 7,
+    total3: 14,
+    total4: 15,
+    total5: 16,
+  },
+  {
+    memoized: true,
+    immutable: false,
+    plugins: [],
+    sub: 3,
+    total1: 7,
+    total2: 7,
+    total3: 14,
+    total4: 15,
+    total5: 16,
+  },
+];
 describe("Evaluate several simple cases and check cache consistency", () => {
-  const l1 = findChild(symTree1, ["left"]);
-  const ll1 = findChild(symTree1, ["left", "left"]);
-  const lr1 = findChild(symTree1, ["left", "right"]);
-  const r1 = findChild(symTree1, ["right"]);
-  const rr1 = findChild(symTree1, ["right", "right"]);
-  const rl1 = findChild(symTree1, ["right", "left"]);
-
-  const l2 = findChild(symTree2, ["left"]);
-  const ll2 = findChild(symTree2, ["left", "left"]);
-  const lr2 = findChild(symTree2, ["left", "right"]);
-  const r2 = findChild(symTree2, ["right"]);
-  const rr2 = findChild(symTree2, ["right", "right"]);
-  const rl2 = findChild(symTree2, ["right", "left"]);
-
-  test.each([
-    {
-      memoized: false,
-      sub: 3,
-      total1: 10,
-      total2: 17,
-      total3: 24,
-      total4: 31,
-      total5: 38,
-    },
-    {
-      memoized: true,
-      sub: 3,
-      total1: 7,
-      total2: 7,
-      total3: 14,
-      total4: 15,
-      total5: 16,
-    },
-  ])(
-    "with memoize set to %p, expect %p subtree evaluations and %p total evaluations",
-    (args) => {
+  describe("Tests on immutable trees", () => {
+    test.each(cases)("case %#", (args) => {
+      const tree1 = { ...symTree1 };
+      const tree2 = { ...symTree2 };
+      const l1 = findChild(tree1, ["left"]);
+      const r2 = findChild(tree2, ["right"]);
       const stats = new CounterPlugin();
-      const tree = new Arbor(symTree1, indexedBinaryChildren, {
-        plugins: [stats],
+      const tree = new Arbor(tree1, indexedBinaryChildren, {
+        plugins: [stats, ...args.plugins],
         reification: { memoize: args.memoized },
-        immutable: true,
+        immutable: args.immutable,
       });
       /** Attach attributes to tree */
       const { min, globmin } = tree.attach(repminCluster);
@@ -84,7 +103,7 @@ describe("Evaluate several simple cases and check cache consistency", () => {
       /**
        * Now let's switch to root altogether.
        */
-      tree.setRoot(symTree2);
+      tree.setRoot(tree2);
 
       /** Switching root shouldn't cause any invocations yet (because this is lazy) */
       expect(stats.invocations(evalMin)).toEqual(args.total2);
@@ -94,14 +113,26 @@ describe("Evaluate several simple cases and check cache consistency", () => {
       expect(stats.invocations(evalMin)).toEqual(args.total3);
 
       /** Let's switch back to the original tree.  The cached values for that should still apply! */
-      tree.setRoot(symTree1);
+      tree.setRoot(tree1);
       /** Now, if we evaluate the root we should get the root min */
       expect(min(tree.root)).toEqual(1);
       /** In the memoized case, no new evaluations are required, otherwise 7 more evaluations will occur */
       expect(stats.invocations(evalMin)).toEqual(args.total4);
 
-      /** Create a new tree that contains nodes from old tree (so cache should be reused) */
-      tree.setRoot(fork(l1, r2));
+      /** How we mutate the tree depends on whether the underlying tree is immutable */
+      if (args.immutable) {
+        /** If immutable, create a new tree that contains nodes from old tree (so cache should be reused) */
+        tree.setRoot(fork(l1, r2));
+      } else {
+        /** If tree is mutable, replace left and right subtrees and inform Arbor of change to node. */
+        const t = tree.root;
+        expect(t.type).toEqual("fork");
+        if (t.type === "fork") {
+          t.left = l1;
+          t.right = r2;
+          tree.update(t);
+        }
+      }
       /** The change in root shouldn't have impacted totals yet */
       expect(stats.invocations(evalMin)).toEqual(args.total4);
 
@@ -110,8 +141,8 @@ describe("Evaluate several simple cases and check cache consistency", () => {
 
       /** In the memoized case, this should only add one new evaluation (the root), otherwise 7 more evaluations will occur */
       expect(stats.invocations(evalMin)).toEqual(args.total5);
-    }
-  );
+    });
+  });
 
   // TODO: Mutable cases
   // - MobX
