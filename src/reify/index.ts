2478e6e242488958eddb45acefb100734c718eff
Michael Tiller <michael.tiller@gmail.com>
2022-01-02 09:29:58 -0500

    Remove MobxReifier from exports

--- src/reify/index.ts
+++ src/reify/index.ts
@@ -1,3 +1,9 @@
 export * from "./reifier";
-export * from "./mobx";
 export * from "./standard";
+
+// I deliberately don't export this.  This allows MobX to be an optional
+// dependency.  If I export it, then it gets pulled in whenever someone uses
+// `arborglyph`.
+// It can still be imported from "arborglyph/lib/reify/mobx".  It could also
+// be turned into an external package.
+// export * from "./mobx";
